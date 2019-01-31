874e8939f6e65a5e4daf3f315f5d72cbd4efe143
Kyle Kingsberry <kkingsbe@gmail.com>
2019-01-31 10:20:28 -0500

    Started Java Array Excercises

---
+++ src/Unit5/JavaArrayExcercises.java
@@ -0,0 +1,16 @@
+package Unit5;
+
+import java.util.Arrays;
+
+public class JavaArrayExcercises {
+    public static int[] sortArray(int[] input)
+    {
+        Arrays.sort(input);
+        return input;
+    }
+    public static string[] sortArray(string[] input)
+    {
+        Arrays.sort(input);
+        return input;
+    }
+}
